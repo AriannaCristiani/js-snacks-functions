b982f310b56db353885328ac0fbc0052ce7788de
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-21 21:38:30 +0200

    risoluzione traccia 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,28 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vocali (){
+
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+for (i=0 ; i < word.length ; i++) {
+    let vocali = word.charAt(i)
+    // console.log(vocali)
 
+    if (vocali === 'a'){
+        console.log(vocali)
+    }  else if (vocali === 'e'){
+        console.log(vocali)
+    } else if (vocali === 'i'){
+        console.log(vocali)
+    } else if (vocali === 'o'){
+        console.log(vocali)
+    } else if (vocali === 'u'){
+        console.log(vocali)
+    } 
+}
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
