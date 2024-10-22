0151317dfa3b32f713d806c86dee26d844ac9d42
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-22 14:39:41 +0200

    correzione eercizio 3 come da lezione

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,28 +5,42 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
-function vocali (){
+function vowels (string) {
+ string = string.toLowerCase();
+ let count = 0;
+
+ for (i=0 ; i < string.length ; i++) {
+   
+    // console.log(string[i])
+
+    const letter = string[i]
+
+    if (letter === 'a'){
+        count = count +=1
+        console.log(letter)
+    }  else if (letter === 'e'){
+        count = count +=1
+        console.log(letter)
+    } else if (letter === 'i'){
+        count = count +=1
+        console.log(letter)
+    } else if (letter === 'o'){
+        count = count +=1
+        console.log(letter)
+    } else if (letter === 'u'){
+        count = count +=1
+        console.log(letter)
+    }
+    
+}
+
+return count;
 
 }
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-for (i=0 ; i < word.length ; i++) {
-    let vocali = word.charAt(i)
-    // console.log(vocali)
-
-    if (vocali === 'a'){
-        console.log(vocali)
-    }  else if (vocali === 'e'){
-        console.log(vocali)
-    } else if (vocali === 'i'){
-        console.log(vocali)
-    } else if (vocali === 'o'){
-        console.log(vocali)
-    } else if (vocali === 'u'){
-        console.log(vocali)
-    } 
-}
+console.log(vowels(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
